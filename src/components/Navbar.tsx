61fde5f601828bcf37439ea3e746d7eb62ff218b
Leandro Emanuel Diaz <leandrodiaz042@gmail.com>
2024-06-24 19:51:46 -0300

    updates

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,23 @@
+import { Link } from "next-view-transitions";
+
+const pages = [
+  { title: "Image Optimization", href: "/image-optimization" },
+  { title: "Theme Toggle", href: "/theme-toggle" },
+];
+
+const Navbar = () => {
+  return (
+    <nav className="bg-red-50 text-black flex items-center justify-center gap-20 p-2">
+      <Link href="/">Home</Link>
+
+      <ul className="flex gap-5">
+        {pages.map((l) => (
+          <Link key={l.href} href={l.href}>
+            {l.title}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+export default Navbar;
